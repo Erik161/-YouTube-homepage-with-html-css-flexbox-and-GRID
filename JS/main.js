df5267ee080dcea60b980228440cb53abf8b6ba9
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-03-23 22:12:21 -0600

    |MediaQueries|Responsive Design|

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,18 @@ const conteneder = document.querySelector('#contenedor');/*Indicamos que acceda 
 document.querySelector('#boton-menu').addEventListener('click', ()=>{/*Indicamos que acceda al ID boton-menu y escuche un click */
     conteneder.classList.toggle('activate');/*Ingresa a conteneder y accedemos a la lista de clases e indicamos que haga un toogle quita o pone activate*/
 });
+
+const comprobarAncho=()=>{
+     if(window.innerWidth <=768){
+        contenedor.classList.remove('activate')
+    } else{
+        contenedor.classList.add('activate');
+    }
+}
+
+comprobarAncho();
+
+
+window.addEventListener('resize', ()=>{
+    comprobarAncho();
+})
